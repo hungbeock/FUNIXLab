0a301103942752e6def81ff37c0689fbd5bea25a
hungbeock <hungbeock2@gmail.com>
2022-03-22 21:35:00 +0900

    hoanthanh

--- my-app/src/components/ContactComponent.js
+++ my-app/src/components/ContactComponent.js
@@ -1,7 +1,10 @@
-import React ,{Component} from 'react';
-import {Breadcrumb , BreadcrumbItem ,Input,Form,FormGroup,Label,Col ,Button ,Row ,FormFeedback} from 'reactstrap'
+import React, { Component } from 'react';
+import {
+    Breadcrumb, BreadcrumbItem,
+    Button, Col, Label, Row
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
-import {Control ,LocalForm ,Errors} from 'react-redux-form';
+import { Control, LocalForm, Errors, actions } from 'react-redux-form';
 
 class Contact extends Component  {
     constructor(props) {
@@ -66,6 +69,7 @@ class Contact extends Component  {
                         <h3>Send us Your Feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
+                   
                     <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
