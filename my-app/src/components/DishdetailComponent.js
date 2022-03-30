03b8df672f143383753b6ea1db3f2af5aef2fa38
hungbeock <hungbeock2@gmail.com>
2022-03-30 21:36:47 +0900

    redux-thunk

--- my-app/src/components/DishdetailComponent.js
+++ my-app/src/components/DishdetailComponent.js
@@ -18,6 +18,7 @@ import {
 import { Link } from 'react-router-dom';
 import dateFormat from "dateformat";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import {Loading} from './LoadingComponent'
 
 function RenderDish({dish}) {
     console.log("dishes", dish)
@@ -151,7 +152,23 @@ class CommentForm extends React.Component {
   }
 
   const DishDetail = (props) => {
-    if (props.dish != null)
+    if(props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    }else if(props.errMess) {
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+    }
+
+    else if (props.dish != null)
       return (
         <div className="container">
           <div className="row">
--- my-app/src/components/MenuComponent.js
+++ my-app/src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, Breadcrumb, BreadcrumbItem, CardTitle } from 'reactstrap';
 import {Link } from 'react-router-dom'
+import {Loading } from './LoadingComponent'
 //Function used to render Menu Item 
 // Input: dish object 
 function RenderMenuItem ({dish, onClick}) {
@@ -17,13 +18,30 @@ function RenderMenuItem ({dish, onClick}) {
 }
 
 const Menu = (props) => {
-    const menu = props.dishes.map((dish) => {
+    const menu = props.dishes.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
                 <RenderMenuItem dish={dish}  />
             </div>
         );
     });
+    if(props.dishes.isLoading){
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    }else if(props.dishes.errMess) {
+            <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMess}</h4>
+                </div>
+            </div>
+    
+    }   
+    else
 
     return (
         <div className="container">
